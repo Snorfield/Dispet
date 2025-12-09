e47324e6aa57c7a0ad47a41dbc069f04749322a7
S. Norfield <225273460+Snorfield@users.noreply.github.com>
2025-12-09 11:16:23 -0700

    Refactor release

--- commands/release.js
+++ commands/release.js
@@ -1,20 +1,20 @@
 const database = require('../database.js');
 const { simpleEmbed } = require('../utils.js')
+const { getUserInformation } = require('./functions/database-handlers.js')
 
 async function release(interaction) {
 
     await interaction.deferReply();
 
-    let selectUser = database.prepare('SELECT * FROM users WHERE user_id = ?');
     let releaseUser = database.prepare('UPDATE users SET emoji_id = NULL, pet_name = NULL WHERE user_id = ?');
 
-    let userObject = selectUser.get(interaction.user.id);
+    let userObject = getUserInformation(interaction.user.id);
 
     if (userObject && userObject.emoji_id) {
         releaseUser.run(interaction.user.id);
-        await interaction.editReply({ embeds: [simpleEmbed('✅ Your current emoji pet has been released to be on their own! Try using `/adopt` to get a new one.')] });
+        return interaction.editReply({ embeds: [simpleEmbed('✅ Your current emoji pet has been released to be on their own! Try using `/adopt` to get a new one.')] });
     } else {
-        await interaction.editReply({ embeds: [simpleEmbed('❌ You don\'t have an emoji pet to release! Try using `/adopt` to get one.')] });
+        return interaction.editReply({ embeds: [simpleEmbed('❌ You don\'t have an emoji pet to release! Try using `/adopt` to get one.')] });
     }
 }
 
