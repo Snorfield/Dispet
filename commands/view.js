782cb925becf296c58c1d650b2b5c3e73c2f9bf2
S. Norfield <225273460+Snorfield@users.noreply.github.com>
2025-12-09 11:21:18 -0700

    Refactor view function and add card drawing
    Refactor view function to use database handlers and draw card image.

--- commands/view.js
+++ commands/view.js
@@ -1,12 +1,11 @@
-const database = require('../database.js');
-const { simpleEmbed } = require('../utils.js')
+const { simpleEmbed } = require('../utils.js');
+const { getUserInformation, getEmojiInformation } = require('./functions/database-handlers.js');
+const drawCard = require('./functions/card');
+const { AttachmentBuilder } = require('discord.js');
 
 async function view(interaction) {
 
     await interaction.deferReply();
-
-    let selectUser = database.prepare('SELECT * FROM users WHERE user_id = ?');
-    let selectEmoji = database.prepare('SELECT * FROM cache WHERE emoji_id = ?');
 
     let user = interaction.options.getUser('user');
     let userId = null;
@@ -14,31 +13,32 @@ async function view(interaction) {
     userId = (user) ? user.id : interaction.user.id;
     user = (user) ? user : interaction.user;
 
-    let userObject = selectUser.get(userId);
+    let userObject = getUserInformation(userId);
 
     if (userObject && userObject.emoji_id) {
-        const emoji = selectEmoji.get(userObject.emoji_id);
+        const emojiInformation = getEmojiInformation(userObject.emoji_id);
 
-        const tiers = ['Common', 'Rare', 'Epic', 'Legendary', 'Mythical'];
-        const tierColors = [0xAAAAAA, 0x0099FF, 0x9933FF, 0xFFD700, 0xFF4500];
+        const emoji = {
+            owner: user.username,
+            id: emojiInformation.emoji_id,
+            name: emojiInformation.emoji_name,
+            bio: emojiInformation.bio,
+            damage: emojiInformation.damage,
+            tier: emojiInformation.tier,
+            health: emojiInformation.health,
+            abilityName: emojiInformation.ability_name,
+            abilityBio: emojiInformation.ability_bio,
+            animated: emojiInformation.animated
+        }
 
-        await interaction.editReply({
-            embeds: [
-                {
-                    "title": emoji.emoji_name,
-                    "description": `**Tier**: ${tiers[emoji.tier - 1]}\n**Health**: ${emoji.health}\n**Damage**: ${emoji.damage}\n\n*${emoji.bio}*`,
-                    "color": tierColors[emoji.tier - 1],
-                    "author": {
-                        "name": `${user.username}'s`
-                    },
-                    "image": {
-                        "url": `https://cdn.discordapp.com/emojis/${emoji.emoji_id}.${(emoji.animated) ? 'gif' : 'png'}`
-                    }
-                }
-            ]
-        });
+        const image = await drawCard(emoji);
+
+        const attachment = new AttachmentBuilder(image, { name: 'card.png' });
+
+        return interaction.editReply({ files: [attachment] });
+
     } else {
-        await interaction.editReply({ embeds: [simpleEmbed('❌ This user does not have an emoji pet.')] });
+        return interaction.editReply({ embeds: [simpleEmbed('❌ This user does not have an emoji pet.')] });
     }
 }
 
