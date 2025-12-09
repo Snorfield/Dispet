a47c5b95ea8c720078cde2e89d7a187efc4cf935
S. Norfield <225273460+Snorfield@users.noreply.github.com>
2025-12-09 15:42:16 -0700

    Change buffer format from 'png' to 'image/png'

--- commands/functions/card.js
+++ commands/functions/card.js
@@ -109,7 +109,7 @@ async function drawCard(emoji) {
 
     drawTextMultipleLines(emoji.abilityBio, color, shadowColor, 20, 3);
 
-    const buffer = canvas.toBuffer('png');
+    const buffer = canvas.toBuffer('image/png');
 
     return buffer;
 }
