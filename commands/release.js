497453c526604b01a0a9212c8720fceac9830985
PlayfulMathematician <me@playfulmathematician.com>
2025-12-12 11:19:27 +0800

    removed suboptimal code

--- commands/release.js
+++ commands/release.js
@@ -9,13 +9,15 @@ async function release(interaction) {
     let releaseUser = database.prepare('UPDATE users SET emoji_id = NULL, pet_name = NULL WHERE user_id = ?');
 
     let userObject = getUserInformation(interaction.user.id);
-
+    let reply;
     if (userObject && userObject.emoji_id) {
         releaseUser.run(interaction.user.id);
-        return interaction.editReply({ embeds: [simpleEmbed('✅ Your current emoji pet has been released to be on their own! Try using `/adopt` to get a new one.')] });
+        reply = '✅ Your current emoji pet has been released to be on their own! Try using `/adopt` to get a new one.';
     } else {
-        return interaction.editReply({ embeds: [simpleEmbed('❌ You don\'t have an emoji pet to release! Try using `/adopt` to get one.')] });
+        reply = '❌ You don\'t have an emoji pet to release! Try using `/adopt` to get one.';
     }
+    return interaction.editReply({ embeds: [simpleEmbed(reply)] });
 }
 
 module.exports = release;
+
